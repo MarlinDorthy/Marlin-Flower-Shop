c88fda287c3ec21adda067dd731fc9287ee01b94
MarlinDorthy <137144619+MarlinDorthy@users.noreply.github.com>
2024-07-31 11:42:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('shopNowBtn').addEventListener('click', function() {
+    alert('Shop Now button clicked!');
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+});
